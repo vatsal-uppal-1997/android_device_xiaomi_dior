dbcaa4d90408990cc7fcb1432699de695e8c9ad8
Faheem Kamal <faheem.kamal.a@gmail.com>
2015-11-08 02:30:50 +0600

    Update omni.mk

--- omni.mk
+++ omni.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/dior/full_dior.mk)
