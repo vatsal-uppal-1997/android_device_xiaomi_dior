7e2b5cc8f49a3eb48e0bacdc64f053ad5158c80e
Faheem Kamal <opodartho@gmail.com>
2015-11-23 01:08:34 +0600

    fixup noob mistake of not editing the init makefile properly

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,33 +12,33 @@ include $(BUILD_PREBUILT)
 # Init scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.armani
+LOCAL_MODULE       := fstab.dior
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.armani
+LOCAL_SRC_FILES    := etc/fstab.dior
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.armani.usb.rc
+LOCAL_MODULE       := init.dior.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.armani.usb.rc
+LOCAL_SRC_FILES    := etc/init.dior.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.armani.rc
+LOCAL_MODULE       := init.dior.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.armani.rc
+LOCAL_SRC_FILES    := etc/init.dior.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.armani.rc
+LOCAL_MODULE       := ueventd.dior.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.armani.rc
+LOCAL_SRC_FILES    := etc/ueventd.dior.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
