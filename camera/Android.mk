d80b763011824f514968ff6070f22783bcbaf80c
Faheem Kamal <opodartho@gmail.com>
2015-11-28 21:37:30 +0600

    fix build errors

--- camera/Android.mk
+++ camera/Android.mk
@@ -5,11 +5,13 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils
+    libhardware liblog libcamera_client libutils
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.msm8226
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
 
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
